b48cd6957799cf385416f7b181ba113500a89770
denisafilip <denifilip02@yahoo.com>
2022-05-12 19:56:40 +0300

    develop: Add backend backbone structure

---
+++ src/main/java/com/example/perspective/PerspectiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.perspective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerspectiveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerspectiveProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/perspective/PerspectiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perspective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerspectiveProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
